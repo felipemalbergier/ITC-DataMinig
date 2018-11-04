60a708680c5266bc258e5f659e61548de725081c
shalom <shalomazar@gmail.com>
2018-11-04 11:04:18 +0200

    terminal arguement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import click
 
 BASE_URL = "https://www.investopedia.com"
 NEWS_URL = r'/news/?page={}'
@@ -14,22 +15,31 @@ def url_to_soup(url):
     return soup
 
 
-def parse_page_information(url):
+def parse_page_information(url, title, date, author, content, describe):
     """Prints (for now) the article information in the page."""
     soup_news = url_to_soup(url)
     
-    title = soup_news.find('h1').text.strip()
-    date = soup_news.find('meta', {'property': "article:published_time"})['content']
-    author = soup_news.find('span', {'class':'by-author'}).a.text
-    content = soup_news.find('div', {'class':'content-box'})
-    
-    print('''
-    URL: {url}
-    Title: {title}
-    Date: {date}
-    Author: {author}
-    Len of Content: {len_content}
-    '''.format(url=url, title=title, date=date, author=author, len_content=len(content.text)))
+    title_string = soup_news.find('h1').text.strip()
+    date_string = soup_news.find('meta', {'property': "article:published_time"})['content']
+    author_string = soup_news.find('span', {'class': 'by-author'}).a.text
+    content_string = soup_news.find('div', {'class': 'content-box'})
+
+    if describe:
+        title = True
+        date = True
+        author = True
+        content = True
+
+    print("-"*40)
+    print('URL: {url}'.format(url=url))
+    if title:
+        print('Title: {title}'.format(title=title_string))
+    if date:
+        print('Date: {date}'.format(date=date_string))
+    if author:
+        print('Author: {author}'.format(author=author_string))
+    if content:
+        print('Len of Content: {len_content}'.format(len_content=len(content_string.text)))
 
 
 def get_list_href(url):
@@ -39,7 +49,13 @@ def get_list_href(url):
     return hrefs
 
 
-def main():
+@click.command()
+@click.option('--title', is_flag=True)
+@click.option('--date', is_flag=True)
+@click.option('--author', is_flag=True)
+@click.option('--content', is_flag=True)
+@click.option('--describe', is_flag=True)
+def main(title, date, author, content, describe):
     URLS_TO_VISIT = []
     URLS_TO_VISIT.append(BASE_URL)
 
@@ -49,14 +65,14 @@ def main():
         while url_to_visit in VISITED_URLS:
             url_to_visit = URLS_TO_VISIT.pop(0)
 
-        print(url_to_visit + '\r', end="")
+        #print(url_to_visit + '\r')
 
         try:
-            parse_page_information(url_to_visit)
+            parse_page_information(url_to_visit, title, date, author, content, describe)
         except AttributeError:
-            print('.', end='')
+            print('.')
         except TypeError:
-            print('.', end='')
+            print('.')
         news_href = get_list_href(url_to_visit)
         URLS_TO_VISIT += news_href
 
