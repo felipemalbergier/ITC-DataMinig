5d6133b70a962b7ab07c234600711ae69641a47b
Felipe <f.malbergier@gmail.com>
2018-11-04 11:08:17 +0200

    Documented and with indexes urls

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def url_to_soup(url):
 
 
 def parse_page_information(url, title, date, author, content, describe):
-    """Prints (for now) the article information in the page."""
+    """Prints (for now) the article information in the page depending on the parameters given."""
     soup_news = url_to_soup(url)
     
     title_string = soup_news.find('h1').text.strip()
@@ -43,6 +43,7 @@ def parse_page_information(url, title, date, author, content, describe):
 
 
 def get_list_href(url):
+    """Returns a list of hrefs links fond on the 'url'. It also concatenate the href with the base url."""
     soup = url_to_soup(url)
     hrefs = soup.find_all('', {'href': True})
     hrefs = [BASE_URL + u['href'] for u in hrefs if re.match(r'/[^/]', u['href'])]
@@ -79,7 +80,6 @@ def main(title, date, author, content, describe):
         VISITED_URLS.add(url_to_visit)
 
 
-
 if __name__ == "__main__":
     main()
 
@@ -92,7 +92,32 @@ Indexes pages:
 - https://www.investopedia.com/investing/investing-basics/education/
 - https://www.investopedia.com/investing/investing-basics/strategy/
 - https://www.investopedia.com/tech/robo-advisor/
-- 
-- 
-
+- https://www.investopedia.com/investing/bonds-and-fixed-income/all/
+- https://www.investopedia.com/investing/bonds-and-fixed-income/education/
+- https://www.investopedia.com/investing/financial-analysis/all/
+- https://www.investopedia.com/investing/financial-analysis/education/
+- https://www.investopedia.com/investing/economics/
+- https://www.investopedia.com/small-business/
+- https://www.investopedia.com/personal-finance/insurance/
+- https://www.investopedia.com/personal-finance/credit-loans-mortgages/
+- https://www.investopedia.com/personal-finance/entrepreneurship/
+- https://www.investopedia.com/personal-finance/taxes/
+- https://www.investopedia.com/personal-finance/real-estate/
+- https://www.investopedia.com/personal-finance/budgeting-savings/
+- https://www.investopedia.com/wealth-management/personal-wealth-and-private-banking/
+- https://www.investopedia.com/wealth-management/estate-planning/
+- https://www.investopedia.com/wealth-management/real-estate/
+- https://www.investopedia.com/wealth-management/insurance/
+- https://www.investopedia.com/wealth-management/tax-strategy/
+- https://www.investopedia.com/wealth-management/high-net-worth-living/
+- https://www.investopedia.com/financial-advisor/mutual-funds/
+- https://www.investopedia.com/financial-advisor/retirement/
+- https://www.investopedia.com/financial-advisor/your-clients/
+- https://www.investopedia.com/financial-advisor/your-practice/
+- https://www.investopedia.com/financial-advisor/financial-advisor-technology/
+- https://www.investopedia.com/active-trading/trading-strategy-fundamentals/all/
+- https://www.investopedia.com/active-trading/trading-strategy-fundamentals/education/
+- https://www.investopedia.com/active-trading/options-and-futures/education/
+- https://www.investopedia.com/active-trading/options-and-futures/instruments/
+- https://www.investopedia.com/tech/
 """
